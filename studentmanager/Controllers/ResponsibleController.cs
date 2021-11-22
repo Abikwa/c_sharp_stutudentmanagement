334cb423b326933ec45a10d8e4a4c4f6efe95246
abikwa <abikalex8@gmail.com>
2021-11-22 14:43:09 +0200

    add responsible controller

---
+++ studentmanager/Controllers/ResponsibleController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace studentmanager.Controllers
+{
+    public class ResponsibleController : Controller
+    {
+        //
+        // GET: /Responsible/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
